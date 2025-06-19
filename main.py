40edc0ec6458eedcb36d1cb0121e2ad230a7a6d0
ZikrullaRaxmatov <zikrulloraxmatov98@gmail.com>
2025-06-19 17:10:34 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,72 @@
+import re
+import cv2
+import pytesseract
+
+path = './video_right.mp4'
+
+cap = cv2.VideoCapture(path)
+
+count_img = 0
+
+if not cap.isOpened():
+    print("Camera not accessible or not found.")
+else:
+    print("Camera is working! Press 'q' to quit.")
+    while True:
+        ret, img = cap.read()
+        if not ret:
+            print("Failed to grab frame")
+            break
+        
+        if count_img % 5 ==0:
+
+            # Convert to grayscale for better OCR results
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            # Optional: thresholding or blurring
+            gray = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)[1]
+
+            # Extract text from preprocessed image
+            text = pytesseract.image_to_string(gray)
+
+            # Example OCR result
+            ocr_text = text.replace('\n', ' ').strip()
+
+            print(ocr_text)
+
+            # Extract Passport Number (e.g., FA6752048)
+            passport_number = re.search(r'\b[A-Z]{2}\d{7}\b', ocr_text)
+
+            if passport_number:
+                print("Passport No:", passport_number.group())
+                break
+
+            # Step 1: Find all MRZ-like lines (at least 40 characters, all uppercase/number/<)
+            mrz_lines = re.findall(r'[A-Z0-9<]{40,}', ocr_text)
+            print(mrz_lines)
+            print(len(mrz_lines))
+
+            # Step 2: If second line exists, extract it and get passport number
+            if mrz_lines:
+                mrz_line = mrz_lines[0]
+                print("MRZ Line:", mrz_line)
+
+                # Step 3: Extract passport number (2 letters + 7 digits)
+                match = re.search(r'[A-Z]{2}\d{7}', mrz_line)
+                if match:
+                    print("Passport No:", match.group())
+                    break
+                else:
+                    print("Passport No not found in MRZ Line")
+            else:
+                print("MRZ Line not found.", count_img)
+            
+        count_img += 1
+
+        #cv2.imshow("Camera Test", img)
+
+        #if cv2.waitKey(1) & 0xFF == ord('q'):
+            #break
+
+#cap.release()
+#cv2.destroyAllWindows()
